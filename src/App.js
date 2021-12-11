e3410a485e4f45732fd5af20bd893d6ea5194e17
Nguyen Thuan Hung <e0550385@u.nus.edu>
2021-12-11 23:02:51 +0800

    Sign in works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import NavBar from './components/navbar';
 import SubjectInfo from './components/subjectInfoBar';
 import InfoConsoles from './components/infoConsoles';
+import SignInScreen from './components/signIn';
 
 function App() {
   return (
     <div>
+      <SignInScreen />
+
       <NavBar />
 
       <SubjectInfo />
