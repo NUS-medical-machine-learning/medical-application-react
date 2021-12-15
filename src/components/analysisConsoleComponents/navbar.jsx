38e6ec156ecf898183924a88c607d7699e494898
Nguyen Thuan Hung <e0550385@u.nus.edu>
2021-12-15 12:32:59 +0800

    Sign In Page

--- src/components/analysisConsoleComponents/navbar.jsx
+++ src/components/analysisConsoleComponents/navbar.jsx
@@ -7,10 +7,10 @@ class NavBar extends Component {
     return (
       <section className="">
         <div className="container">
-          <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <header className="d-flex justify-content-center py-3 mb-4 border-bottom">
             <a
               href="/"
-              className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+              className="d-flex align-items-center mb-3 me-md-auto text-dark text-decoration-none"
             >
               <svg className="bi me-2" width={40} height={32}>
                 <use xlinkHref="#bootstrap" />
