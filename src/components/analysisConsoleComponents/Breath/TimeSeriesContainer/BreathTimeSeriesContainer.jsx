7c39859742abef059d132db35c8452a4cb8ada6b
P4Q-260-PC\Tofuser <n/a>
2021-12-16 21:22:24 +0800

    SuggestedMax

--- src/components/analysisConsoleComponents/Breath/TimeSeriesContainer/BreathTimeSeriesContainer.jsx
+++ src/components/analysisConsoleComponents/Breath/TimeSeriesContainer/BreathTimeSeriesContainer.jsx
@@ -29,7 +29,6 @@ ChartJS.register(
 );
 
 const options = {
-  spanGaps: 1000 * 20,
   responsive: true,
   plugins: {
     legend: {
@@ -45,7 +44,22 @@ const options = {
       time: {
         unit: "second",
       },
+      title: {
+        display: true,
+        text: 'Date'
+      },
+      ticks: {
+        autoSkip: false,
+        maxRotation: 0,
+        major: {
+          enabled: true
+        },
+      }
     },
+    y: {
+      // the data maximum used for determining the ticks is Math.max(dataMax, suggestedMax)
+      suggestedMax: 30,
+    }
   },
 };
 
@@ -83,7 +97,7 @@ const removeData = (chart) => {
 };
 
 const updateData = (chart, label, data) => {
-  if (chart.data.labels.length > 30) { // Render 30 seconds
+  if (chart.data.labels.length > 20) { // Render about 10 seconds
     removeData(chart);
   } 
   addData(chart, label, data);
