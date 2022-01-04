79b530fff5ce705e891acb44641b502d395e92ee
Nguyen Thuan Hung <e0550385@u.nus.edu>
2022-01-04 16:29:54 +0800

    Test send data 4

--- src/components/analysisConsoleComponents/controlButtons.jsx
+++ src/components/analysisConsoleComponents/controlButtons.jsx
@@ -273,8 +273,11 @@ const uploadDataToDummyServer = (props, startingTime) => {
           let recordData = new FormData();
           recordData.append("id", props.getFullSubjectId());
           recordData.append("time", startingTime);
-          console.log("recordData", recordData.values());
 
+          for (let value of recordData.values()) {
+            console.log(value);
+          }
+          
           fetch("http://127.0.0.1:8001/record", {
             method: "POST",
             body: recordData,
