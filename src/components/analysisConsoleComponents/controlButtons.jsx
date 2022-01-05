04d384f8e34cc3bf4e505cf325b826becbf413c0
Nguyen Thuan Hung <e0550385@u.nus.edu>
2022-01-05 21:42:32 +0800

    Finished fake 2

--- src/components/analysisConsoleComponents/controlButtons.jsx
+++ src/components/analysisConsoleComponents/controlButtons.jsx
@@ -302,6 +302,7 @@ const uploadDataToDummyServer = (props, startingTime) => {
 
           ToastDataSent.success(id);
           props.setTestingProgressState(TestingProgress.DataSent);
+          PopUpResult(props);
         });
       })
       .catch((error) => {
